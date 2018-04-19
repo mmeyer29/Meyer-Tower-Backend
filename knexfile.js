ecb493801cc2a152408d913e4e6ba24e369bb070
Matt Meyer <matthew.meyer29@gmail.com>
2018-04-19 11:41:03 -0600

    updated knex stuff

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: "postgres://localhost/starships"
+  },
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
 
 };
